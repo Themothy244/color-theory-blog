2416e74094f96742f3a86de0cb90a22aac147c4c
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-03 16:10:29 +0800

    style(layout): add global light/dark background and text colors

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,9 @@ import './styles/App.css'
 function App() {
 
   return (
-    <>
+    <div className='min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white'>
       <Navbar />
-    </>
+    </div>
   )
 }
 
