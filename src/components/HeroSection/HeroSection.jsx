d9d2440c0d654b38c02bc0448670ade26e431bb7
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-09 22:27:51 +0800

    feat(hero): add responsive styles for sm, md, lg, and xl screens

--- src/components/HeroSection/HeroSection.jsx
+++ src/components/HeroSection/HeroSection.jsx
@@ -2,14 +2,15 @@ import colorWheel from "../../assets/img/color-wheel-1.png";
 
 function HeroSection() {
     return(
-        <div className="mt-20 flex flex-wrap justify-center items-center relative">
-            <div className="mx-[14vw] mt-4 max-w-md">
-                <h1 className="text-[10vw] leading-tight font-helveticaBold font-bold">Understanding Color Theory: A Beginner’s Guide</h1>
+        <div className="mt-20 flex flex-wrap justify-center items-center relative xl:mt-28">
+            <div className="mx-[12vw] mt-4 max-w-md sm:max-w-lg sm:my-6 lg:mx-6 lg:max-w-md xl:mx-12 xl:max-w-lg">
+                <h1 className="text-[clamp(1rem,11vw,56px)] leading-tight font-helveticaBold font-bold 
+                                sm:text-5xl sm:text-center md:text-5xl lg:text-6xl lg:text-left xl:text-7xl">Understanding Color Theory: A Beginner’s Guide</h1>
             </div>
 
-            <img src={colorWheel} alt="Color Wheel" className="w-4/5 max-w-md m-4"/>
+            <img src={colorWheel} alt="Color Wheel" className="w-4/5 max-w-md m-4 sm:w-3/4 lg:w-1/3 xl:mx-12"/>
             
-            <div className="bg-gray-100/90 backdrop-blur-lg rounded-2xl mt-8 mx-[10vw] p-4 max-w-xl absolute top-2/3
+            <div className="bg-gray-100/90 backdrop-blur-lg rounded-2xl mt-8 mx-[10vw] p-4 max-w-lg absolute top-2/3 lg:top-3/4 xl:top-[80%] xl:max-w-2xl
                             dark:bg-gray-800/80">
                 <p className="font-helveticaLight">
                     Whether you're a painter, digital artist, designer, or just 
