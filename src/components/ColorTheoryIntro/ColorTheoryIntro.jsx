0f0c419ecd9797a8302869dbe44335fbcef51a3d
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-12 18:44:41 +0800

    style(color): style header section in ColorTheoryInfo

--- src/components/ColorTheoryIntro/ColorTheoryIntro.jsx
+++ src/components/ColorTheoryIntro/ColorTheoryIntro.jsx
@@ -1,18 +1,19 @@
+import icon from '../../assets/icons/color-palette.svg';
 
 function ColorTheoryIntro() {
     return(
-        <div>
-            <div>
-                <h3>What is</h3>
-                <h1>Color Theory</h1>
-                <div>
-                    <div></div>
-                    <img src="#" alt="icon"/>
-                    <div></div>
+        <div className='my-16 py-16 lg:my-40'>
+            <div className='justify-items-center'>
+                <h3 className='font-helveticaBold font-bold text-2xl'>What is</h3>
+                <h1 className='font-helveticaBold font-bold text-4xl'>Color Theory</h1>
+                <div className='flex flex-row items-center justify-center w-full'>
+                    <div className='h-[1.5px] w-1/3 max-w-md bg-gray-900 dark:bg-white'></div>
+                    <img src={icon} alt="icon" width={50} className='m-4'/>
+                    <div className='h-[1.5px] w-1/3 max-w-md bg-gray-900 dark:bg-white'></div>
                 </div>
             </div>
             <div>
-                <p>
+                <p className='font-helveticaLight'>
                     Color theory is the science and art of using color. It helps 
                     us understand how colors work together, how they affect our 
                     emotions, and how to create visually pleasing combinations.
