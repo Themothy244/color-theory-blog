a5db1a9cab2c07dfc0fe52ba49c0eb47e83ca566
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-12 16:24:25 +0800

    content(color): add raw HTML for ColorTheoryIntro section

--- src/components/ColorTheoryIntro/ColorTheoryIntro.jsx
+++ src/components/ColorTheoryIntro/ColorTheoryIntro.jsx
@@ -2,7 +2,23 @@
 function ColorTheoryIntro() {
     return(
         <div>
-
+            <div>
+                <h3>What is</h3>
+                <h1>Color Theory</h1>
+                <div>
+                    <div></div>
+                    <img src="#" alt="icon"/>
+                    <div></div>
+                </div>
+            </div>
+            <div>
+                <p>
+                    Color theory is the science and art of using color. It helps 
+                    us understand how colors work together, how they affect our 
+                    emotions, and how to create visually pleasing combinations.
+                </p>
+                <p>In simple terms, it’s the why and how behind the colors you choose in your artwork.</p>
+            </div>
         </div>
     );
 }
