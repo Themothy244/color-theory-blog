9429818ba6e15813b3f576987eb42470b42fa01b
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-12 16:07:44 +0800

    feat(app): add <HeroSection /> component to main layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
+import HeroSection from './components/HeroSection/HeroSection'
 import Navbar from './components/Navbar/Navbar'
 import './styles/App.css'
 
 function App() {
 
   return (
-    <div className='min-h-screen bg-white text-black dark:bg-gray-900 dark:text-white'>
+    <div className='min-h-screen p-[1px] bg-white text-black dark:bg-gray-900 dark:text-white'>
       <Navbar />
+      <HeroSection />
     </div>
   )
 }
