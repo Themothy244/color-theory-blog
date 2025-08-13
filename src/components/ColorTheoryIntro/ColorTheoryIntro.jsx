8207cba8122cd28d6ee0ea2318dfdd96be78f648
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-13 18:14:04 +0800

    feat(colro): add color wheel background with spin animation on ColorTheoryInfo.jsx component

--- src/components/ColorTheoryIntro/ColorTheoryIntro.jsx
+++ src/components/ColorTheoryIntro/ColorTheoryIntro.jsx
@@ -1,8 +1,9 @@
 import icon from '../../assets/icons/color-palette.svg';
+import colorWheel from '../../assets/img/color-wheel-2.png'
 
 function ColorTheoryIntro() {
     return(
-        <div className='my-16 py-16 lg:my-40'>
+        <div className='my-16 py-16 lg:my-40 relative'>
             <div className='justify-items-center'>
                 <h3 className='font-helveticaBold font-bold text-2xl'>What is</h3>
                 <h1 className='font-helveticaBold font-bold text-4xl'>Color Theory</h1>
@@ -12,6 +13,11 @@ function ColorTheoryIntro() {
                     <div className='h-[1.5px] w-1/3 max-w-md bg-gray-900 dark:bg-white'></div>
                 </div>
             </div>
+
+            <div className='w-full max-w-sm aspect-square rounded-full overflow-hidden sm:w-3/4 lg:w-1/3 xl:mx-12 absolute top-1/3 right-1/2 z-0 md:right-[58%]'>
+                <img src={colorWheel} alt='color wheel' className='w-full h-full object-cover animate-spinn'/>
+            </div>
+
             <div className='bg-gray-200/80 backdrop-blur-lg mx-[10vw] my-5 rounded-2xl p-5 max-w-3xl lg:mx-auto md:p-6
                             dark:bg-gray-800/80'>
                 <p className='font-helveticaLight mb-3 md:text-lg'>
@@ -21,6 +27,7 @@ function ColorTheoryIntro() {
                     In simple terms, color theory is the reason behind the colors you choose in your artwork. It helps you make better design decisions by showing how to mix and match colors in a way that feels natural and visually pleasing.
                 </p>
             </div>
+        
         </div>
     );
 }
