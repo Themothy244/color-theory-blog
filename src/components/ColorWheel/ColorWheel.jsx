6e351d4f87111dade637fae994b3e1e25b3dbb47
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-14 21:54:59 +0800

    style(colorWheel): style the ColorWheel.jsx component with responsive layout

--- src/components/ColorWheel/ColorWheel.jsx
+++ src/components/ColorWheel/ColorWheel.jsx
@@ -2,14 +2,14 @@ import colorWheel from '../../assets/img/color-wheel-1.png'
 
 function ColorWheel() {
     return(
-        <div>
-            <div>
-                <h1>The Color Wheel</h1>
-                <div></div>
-            </div>
-            <div>
-                <div>
-                    <p>
+        <div className='mx-[8vw] my-16 max-w-fit bg-gray-200/80 backdrop-blur-lg rounded-2xl p-5 lg:mx-auto
+                        dark:bg-gray-800/80'>
+            <div className='flex flex-wrap justify-center items-center lg:m-7'>
+                <div className='sm:my-3 sm:mx-5 lg:max-w-md'>
+                    <h1 className='font-helveticaBold font-bold text-3xl sm:text-4xl lg:text-5xl'>The Color Wheel</h1>
+                    <div className='h-[1.5px] w-3/4 max-w-md my-3 bg-gray-900 dark:bg-white'></div>
+
+                    <p className='font-helveticaLight sm:mx-3 md:text-lg'>
                         The color wheel is a circular chart that shows how 
                         colors relate to one another. It includes primary, 
                         secondary, and tertiary colors, helping you understand 
@@ -20,7 +20,7 @@ function ColorWheel() {
                 </div>
                 <div>
                     <div>
-                        <img src={colorWheel} alt="color wheel" />
+                        <img src={colorWheel} alt="color wheel" className='w-full max-w-xs my-5'/>
                     </div>
                 </div>
             </div>
