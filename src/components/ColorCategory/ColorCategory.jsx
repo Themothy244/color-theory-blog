2caa648da36691820b0ab71cfc747ef8d2da9fdc
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-16 21:49:51 +0800

    content(category): add raw HTML for the ColorCategory section

--- src/components/ColorCategory/ColorCategory.jsx
+++ src/components/ColorCategory/ColorCategory.jsx
@@ -1,8 +1,45 @@
 function ColorCategory() {
     return(
-        <div>
+        <>
+            <div>
+                <div>
+                    <div>
+                        <h1>Primary Colors</h1>
+                        <h3>Red, Yellow, Blue</h3>
+                        <p>
+                            These are the base colors that can’t be created by mixing others.
+                        </p>
+                    </div>
+                    <img src="#"/>
+                </div>
+            </div>
 
-        </div>
+            <div>
+                <div>
+                    <div>
+                        <h1>Secondary Colors</h1>
+                        <h3>Green, Orange, Purple</h3>
+                        <p>
+                            These are created by mixing two primary colors.
+                        </p>
+                    </div>
+                    <img src="#"/>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <div>
+                        <h1>Tertiary Colors</h1>
+                        <h3>Red-Orange, Yellow-Green, Blue-Violet, etc.</h3>
+                        <p>
+                            These are made by mixing a primary and a secondary color.
+                        </p>
+                    </div>
+                    <img src="#"/>
+                </div>
+            </div>
+        </>
     );
 }
 
