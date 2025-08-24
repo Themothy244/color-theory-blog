204b5f6656b67f48a0706f48cd961ee5fe2c6533
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-24 20:27:24 +0800

    style(combination): style header section in ColorCombination.jsx

--- src/components/ColorCombination/ColorCombination.jsx
+++ src/components/ColorCombination/ColorCombination.jsx
@@ -5,14 +5,14 @@ import triadic from '../../assets/img/Triadic-wheel.png';
 
 function ColorCombination() {
     return(
-        <div>
-            <div>
-                <h1>Color Harmonies</h1>
-                <h3>What works together</h3>
-                <div>
-                    <div></div>
-                    <img src={colorWheel} alt="icon" className='w-[50px]'/>
-                    <div></div>
+        <div className='my-40'>
+            <div className='text-center'>
+                <h1 className='font-helveticaBold text-bold text-4xl'>Color Harmonies</h1>
+                <h3 className='font-helveticaLight'>What works together</h3>
+                <div className='flex justify-center items-center'>
+                    <div className='h-[1.5px] w-1/3 max-w-md bg-gray-900 dark:bg-white'></div>
+                    <img src={colorWheel} alt="icon" className='w-[60px] m-4'/>
+                    <div className='h-[1.5px] w-1/3 max-w-md bg-gray-900 dark:bg-white'></div>
                 </div>
             </div>
             <div>
@@ -26,7 +26,8 @@ function ColorCombination() {
                         <h1>Analogous</h1>
                         <p>Colors that are next to each other (e.g., red, red-orange, orange). Calm and natural.</p>
                         <img src={analogous} className='w-96'/>
-                    </div><div>
+                    </div>
+                    <div>
                         <h1>Triadic</h1>
                         <p>Three colors evenly spaced (e.g., red, yellow, blue). Vibrant and balanced.</p>
                         <img src={triadic} className='w-96'/>
