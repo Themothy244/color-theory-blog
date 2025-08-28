3e50537b156ca10611b55e0a63d0c0dfe6647af1
Themothy James <themothyjamesmaloma@gmail.com>
2025-08-28 18:04:52 +0800

    content(WarmCool): add raw HTML for the WarmCool section

--- src/components/WarmCool/WarmCool.jsx
+++ src/components/WarmCool/WarmCool.jsx
@@ -2,7 +2,28 @@
 function WarmCool() {
     return(
         <div>
-            
+            <div>
+                <h3>Warm vs Cool</h3>
+                <div>
+                    <div></div>
+                    <h1>Colors</h1>
+                    <div></div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h1>Warm Colors</h1>
+                    <p>Ex. Red, orange, yellow</p>
+                    <p>Energetic, passionate, attention-grabbing</p>
+                    <img src="#" alt="Warm colors"/>
+                </div>
+                <div>
+                    <h1>Cool Colors</h1>
+                    <p>Ex. Blue, green, purple</p>
+                    <p>Calm, soothing, introspective</p>
+                    <img src="#" alt="Cool colors"/>
+                </div>
+            </div>
         </div>
     );
 }
