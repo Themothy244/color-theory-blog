49ad44bda6f33dba672cc784db9a6cdee1bfcca1
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-03 20:11:23 +0800

    style(tips): style ColorTips.jsx component with responsive layout

--- src/components/ColorTips/ColorTips.jsx
+++ src/components/ColorTips/ColorTips.jsx
@@ -3,30 +3,30 @@ import spectrum from '../../assets/img/color-spectrum.png';
 
 function ColorTips() {
     return(
-        <div>
+        <div className='my-40'>
             <div className='text-center'>
                 <h3 className='font-helveticaLight text-xl'>Experimenting with</h3>
-                <h1 className='font-helveticaBold text-4xl my-2 mx-3'>Colors: Beginner Tips</h1>
+                <h1 className='font-helveticaBold font-bold text-4xl my-2 mx-3'>Colors: Beginner Tips</h1>
                 <div className='flex justify-center items-center'>
                     <div className='h-[1.5px] w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
                     <img src={icon} alt="icon" width={50} className='m-3'/>
                     <div className='h-[1.5px] w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
                 </div>
             </div>
-            <div>
-                <div>
-                    <h1>Start with limited palettes</h1>
-                    <p>Using just 2–3 colors helps you learn control.</p>
+            <div className='bg-gray-200/80 dark:bg-gray-800/80 my-4 mx-[8vw] p-6 rounded-2xl flex justify-center items-center flex-col-reverse max-w-4xl md:flex-row md:justify-between lg:mx-auto'>
+                <div className='md:w-1/2'>
+                    <h1 className='font-helveticaBold font-bold text-xl sm:text-2xl'>Start with limited palettes</h1>
+                    <p className='font-helveticaLight mx-3 text-sm sm:text-base'>Using just 2–3 colors helps you learn control.</p>
 
-                    <h1>Play with contrast</h1>
-                    <p>Use light vs. dark or warm vs. cool to guide the eye.</p>
+                    <h1 className='font-helveticaBold font-bold text-xl sm:text-2xl mt-5'>Play with contrast</h1>
+                    <p className='font-helveticaLight mx-3 text-sm sm:text-base'>Use light vs. dark or warm vs. cool to guide the eye.</p>
 
-                    <h1>Observe nature and other artists</h1>
-                    <p>Take notes on how they use color.</p>
+                    <h1 className='font-helveticaBold font-bold text-xl sm:text-2xl mt-5'>Observe nature and other artists</h1>
+                    <p className='font-helveticaLight mx-3 text-sm sm:text-base'>Take notes on how they use color.</p>
 
-                    <p>Use digital tools like Adobe Color or Coolors.co for color scheme ideas.</p>
+                    <p className='font-helveticaLight mt-5 text-sm sm:text-base'>Use digital tools like Adobe Color or Coolors.co for color scheme ideas.</p>
                 </div>
-                <img src={spectrum} alt="color wheel spectrum" />
+                <img src={spectrum} alt="color wheel spectrum" className='mb-10 w-4/5 md:mb-0 sm:max-w-80 md:w-2/5'/>
             </div>
         </div>
     );
