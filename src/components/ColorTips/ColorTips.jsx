bc45c9fdfa25ac665fd2b379b658807b5e5c8e95
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-03 19:21:32 +0800

    content(tips): add raw HTML for ColorTips section

--- src/components/ColorTips/ColorTips.jsx
+++ src/components/ColorTips/ColorTips.jsx
@@ -1,7 +1,33 @@
+import icon from '../../assets/icons/color-palette.svg';
+import spectrum from '../../assets/img/color-spectrum.png';
+
 function ColorTips() {
     return(
         <div>
-            
+            <div>
+                <h3>Experimenting with</h3>
+                <h1>Colors: Beginner Tips</h1>
+                <div>
+                    <div></div>
+                    <img src={icon} alt="icon" />
+                    <div></div>
+                </div>
+            </div>
+            <div>
+                <div>
+                    <h1>Start with limited palettes</h1>
+                    <p>Using just 2–3 colors helps you learn control.</p>
+
+                    <h1>Play with contrast</h1>
+                    <p>Use light vs. dark or warm vs. cool to guide the eye.</p>
+
+                    <h1>Observe nature and other artists</h1>
+                    <p>Take notes on how they use color.</p>
+
+                    <p>Use digital tools like Adobe Color or Coolors.co for color scheme ideas.</p>
+                </div>
+                <img src={spectrum} alt="color wheel spectrum" />
+            </div>
         </div>
     );
 }
