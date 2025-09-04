fc24ac0c2ddd411dd2545237a7cc56c223aa8959
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-04 16:13:54 +0800

    style(warmCool): adjust header bar width in WarmCool.jsx component

--- src/components/WarmCool/WarmCool.jsx
+++ src/components/WarmCool/WarmCool.jsx
@@ -9,9 +9,9 @@ function WarmCool() {
                 <img src={colorWheel} width={50} className='mx-auto my-3'/>
                 <h3 className="font-helveticaLight text-lg">Warm vs Cool</h3>
                 <div className="flex justify-center items-center">
-                    <div className='h-[1.5px] w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
+                    <div className='h-[1.5px] w-1/5 sm:w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
                     <h1 className="font-helveticaBold font-bold mx-4 my-2 text-5xl">Colors</h1>
-                    <div className='h-[1.5px] w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
+                    <div className='h-[1.5px] w-1/5 sm:w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
                 </div>
             </div>
             <div className='flex flex-wrap justify-center items-center mx-[8vw] lg:mx-auto'>
