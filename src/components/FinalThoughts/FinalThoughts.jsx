594e0937addddbbcdf5975f7b0111ad13c451d65
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-04 15:51:17 +0800

    content(thoughts): add raw HTML for FinalThoughts section

---
+++ src/components/FinalThoughts/FinalThoughts.jsx
@@ -0,0 +1,34 @@
+
+function FinalThoughts() {
+    return(
+        <div>
+            <div>
+                <h3>Final Thoughts on</h3>
+                <h1>Color Theory</h1>
+                <div>
+                    <div></div>
+                    <img src="#" alt="icon" />
+                    <div></div>
+                </div>
+            </div>
+
+            <div>
+                <img src="#" alt="color wheel" />
+            </div>
+
+            <div>
+                <p>
+                    Understanding color theory doesn’t mean following 
+                    strict rules—it’s about knowing your tools so you 
+                    can break the rules with intention. With practice, 
+                    you’ll start to choose colors more intuitively and 
+                    expressively.
+                </p>
+                <p>Next Step: Try creating an artwork using just an 
+                    analogous or complementary color scheme!</p>
+            </div>
+        </div>
+    );
+}
+
+export default FinalThoughts;
