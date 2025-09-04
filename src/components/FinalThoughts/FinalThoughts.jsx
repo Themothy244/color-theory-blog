c47009d283f5d8aa43cb3c02c7a493022cdda002
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-04 15:58:36 +0800

    style(thoughts): style header section in FinalThoughts.jsx

--- src/components/FinalThoughts/FinalThoughts.jsx
+++ src/components/FinalThoughts/FinalThoughts.jsx
@@ -1,19 +1,21 @@
+import icon from '../../assets/icons/color-palette.svg';
+import colorWheel from '../../assets/img/color-wheel-2.png';
 
 function FinalThoughts() {
     return(
-        <div>
-            <div>
-                <h3>Final Thoughts on</h3>
-                <h1>Color Theory</h1>
-                <div>
-                    <div></div>
-                    <img src="#" alt="icon" />
-                    <div></div>
+        <div className='my-60'>
+            <div className='text-center'>
+                <h3 className='font-helveticaLight text-xl'>Final Thoughts on</h3>
+                <h1 className='font-helveticaBold font-bold text-4xl my-2 mx-3'>Color Theory</h1>
+                <div className='flex justify-center items-center'>
+                    <div className='h-[1.5px] w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
+                    <img src={icon} alt="icon" width={50} className='m-3'/>
+                    <div className='h-[1.5px] w-1/4 max-w-md bg-gray-900 dark:bg-white'></div>
                 </div>
             </div>
 
             <div>
-                <img src="#" alt="color wheel" />
+                <img src={colorWheel} alt="color wheel" />
             </div>
 
             <div>
