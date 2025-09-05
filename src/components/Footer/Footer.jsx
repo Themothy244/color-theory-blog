541b2f6a289a7301f1a22ce794415b0dc73aeb47
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-05 19:26:53 +0800

    content(footer): add raw HTML for Footer section

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,26 @@
+import icon from '/colour-icon.png'
 
 function Footer(){
     return(
         <div>
-
+            <div>
+                <div>
+                    <img src={icon} alt="icon" />
+                    <h1>About This Blog</h1>
+                </div>
+                <p>
+                    "Understanding Color Theory" helps beginners 
+                    make sense of color through simple, creative, 
+                    and practical tips for art, design, and self-expression.
+                </p>
+            </div>
+            <div>
+                <a href="#" className="m-3 font-helveticaLight">Home</a>
+                <a href="#" className="m-3 font-helveticaLight">Basics</a>
+                <a href="#" className="m-3 font-helveticaLight">Harmonies</a>
+                <a href="#" className="m-3 font-helveticaLight">Emotion</a>
+            </div>
+            <p>&copy; 2025 Understanding Color Theory. All rights reserved.</p>
         </div>
     );
 }
