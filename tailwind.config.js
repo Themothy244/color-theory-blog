c23ed0859ddd104f684ba2e119cdd67c51cff413
Themothy James <themothyjamesmaloma@gmail.com>
2025-09-28 21:44:26 +0800

    feat(tailwind): add custom keyframes and animations to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -17,9 +17,35 @@ export default {
           '0%': { transform: 'rotate(0deg)' },
           '100%': { transform: 'rotate(360deg)' },
         },
+        fadeIn: {
+          '0%' : { opacity: '0' },
+          '100%' : { opacity: '1' },
+        },
+        fadeUp: {
+          '0%' : { opacity: '0' },
+          '25%' : { opacity: '0', transform: 'translateY(30px)' },
+          '100%' : { opacity: '1' },
+        },
+        fadeUpDelay: {
+          '0%' : { opacity: '0', transform: 'translateY(40px)' },
+          '100%' : { opacity: '1' },
+        },
+        fadeSlide: {
+          '0%' : { opacity: '0' },
+          '30%' : { opacity: '0', transform: 'translateX(-30px)' },
+          '100%' : { opacity: '1' },
+        },
+        wExpand: {
+          '0%' : { maxWidth: '0' },
+        },
       },
       animation: {
         spinn: 'spin 20s linear infinite',
+        fadeIn: 'fadeIn 1s ease-out forwards',
+        fadeUp: 'fadeUp 1s ease-out forwards',
+        fadeUpDelay: 'fadeUpDelay 1s ease-out forwards',
+        fadeSlide: 'fadeSlide 1.2s ease-out forwards',
+        wExpand: 'wExpand 1s ease-in forwards',
       },
     },
   },
